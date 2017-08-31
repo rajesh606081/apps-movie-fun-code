778063fe24ef7fbdc3b93c82e31ed021c70d2c9a
antonysudharsang@gmail.com <p0wer0n-1>
2017-08-31 08:53:42 -0700

    test

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -2,12 +2,12 @@
 
 set -e +x
 
-pushd attendee-service-source
+pushd movie-fun
   echo "Packaging JAR"
   ./mvnw clean package -DskipTests
 popd
 
-jar_count=`find attendee-service-source/target -type f -name *.jar | wc -l`
+jar_count=`find movie-fun/target -type f -name *.war | wc -l`
 
 if [ $jar_count -gt 1 ]; then
   echo "More than one jar found, don't know which one to deploy. Exiting"
@@ -16,7 +16,7 @@ fi
 
 
 
-find attendee-service-source/target -type f -name *.jar -exec cp "{}" package-output/attendee-service.jar \;
+find movie-fun/target -type f -name *.jar -exec cp "{}" package-output/moviefun.war \;
 
 echo "Done packaging"
 exit 0
